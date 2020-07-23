af31f3d20905c6271519731581e7f3d3a82ff19a
Alex Moiseev <coding-awm@mail.ru>
2020-07-23 14:21:28 +0600

    сделал поля на кнопку БОЛЬШЕ

--- src/modules/moreSentens.js
+++ src/modules/moreSentens.js
@@ -1,11 +1,23 @@
 const moreSentens = () => {
 
-  const addSentenceBtn = document.querySelector('.add-sentence-btn');
-  const sentensBlock = document.querySelectorAll('.text-center > .row > div');
-  addSentenceBtn.addEventListener('click', () => {
-    addSentenceBtn.style.display = 'none';
-    console.log(sentensBlock);
-  })
-}
+	const addSentenceBtn = document.querySelector('.add-sentence-btn');
+	const sentensBlock = document.querySelectorAll('.text-center > .row > div');
+	addSentenceBtn.addEventListener('click', () => {
+		addSentenceBtn.style.display = 'none';
+		console.log(sentensBlock);
+
+		sentensBlock.forEach(elem => {
+			console.log('elem: ', elem);
+			if (elem.classList.contains('hidden')) {
+				elem.classList.remove('hidden');
+			}
+
+			if (elem.classList.contains('visible-sm-block')) {
+				elem.classList.remove('visible-sm-block');
+			}
+
+		});
+	});
+};
 
 export default moreSentens;
