e3433682a358603fc121035470261ce906592b7f
Alex Moiseev <coding-awm@mail.ru>
2020-07-23 12:03:46 +0600

    сделал попапку popupCallToggle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 // eslint-disable-next-line strict
 'use strict';
 
-
+// проверка на модульность, в коце удалить
 import helo from './modules/inity';
 
 helo('sf');
+
+// попапка  popupCallToggle
+import popupCallToggle from './modules/popupCallToggle';
+
+popupCallToggle();
