1246acf09da862b0377a3bbbd60a7449278e3e27
Alex Moiseev <coding-awm@mail.ru>
2020-07-28 20:25:36 +0600

    сделал директорскую форму

--- src/index.js
+++ src/index.js
@@ -38,15 +38,18 @@ calculator();
 
 // формы
 import sendForms from './modules/sendForms';
-sendForms('.main-form', '.phone-user','.gdg','.main-form-btn');
-sendForms('.capture-form', '.phone-user', '.name_2', '.capture-form-btn');
+sendForms('.main-form', '.phone-user','.gdg');
+sendForms('.capture-form', '.phone-user', '.name_2');
 //две верхнгих формы встроенные в вёрстку. они работают
 
 // нижние формы в попапах
-sendForms('.capture-form-consultation', '.phone-user', '.name_13');
-sendForms('.capture-form-discount', '.phone-user', '.name_11');
-sendForms('.capture-form-check', '.phone-user', '.name_12');
-sendForms('.first-form', '.phone-user', '.name_1', '.capture-form-btn');
+
+import sendFormsModals from './modules/sendFormsModals';
+
+sendFormsModals('.capture-form-consultation', '.phone-user', '.name_13', '.capture-form-btn', '.director-form');
+sendFormsModals('.capture-form-discount', '.phone-user', '.name_11', '.capture-form-btn');
+sendFormsModals('.capture-form-check', '.phone-user', '.name_12', '.discount-btn');
+sendFormsModals('.first-form', '.phone-user', '.name_1', '.capture-form-btn');
 
 
 
